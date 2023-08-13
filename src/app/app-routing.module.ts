83d669b3a05ee80e835324744cbd700f6486d7b1
LuYomayel <l.yomayel@gmail.com>
2023-08-13 21:57:28 +1000

    lista la actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { TableComponent } from './table/table.component';
 import { FairPlayComponent } from './fairplay/fairplay.component';
 const routes: Routes = [
+  { path: '', redirectTo: 'goleadores/torneo/2023/CLAUSURA', pathMatch: 'full' },
   { path: 'goleadores/torneo/:year/:tipo', component: TableComponent },
   { path: 'fairplay/torneo/:year/:tipo', component: FairPlayComponent },
 ];
