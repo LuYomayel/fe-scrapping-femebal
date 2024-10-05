b8dbcdf5877fcae9f33640d6c5ee9a596f762814
LuYomayel <l.yomayel@mgail.com>
2024-10-05 16:25:07 +1000

    aca tambien cambiamos el ambiente

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-goleadores-dev.handball-metropolitano.com/'
+  apiUrl: 'https://api-handball-metropolitano.luciano-yomayel.com/'
   // apiUrl: 'http://164.92.101.253:3950/'
 };
